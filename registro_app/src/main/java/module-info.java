33db4511669723932532aa5a3744f33498c4fd8e
st3v3n030707 <safloreso@uamv.edu.ni>
2026-08-28 16:10:54 -0600

    Agregando TableView funcional y permisos en module-info

--- registro_app/src/main/java/module-info.java
+++ registro_app/src/main/java/module-info.java
@@ -3,8 +3,11 @@ module ni.edu.uam.registro_app {
     requires javafx.fxml;
     requires static lombok;
 
-
-
+    // Permite que la vista cargue los controladores
     opens ni.edu.uam.registro_app.controllers to javafx.fxml;
+
+    // NUEVA LÍNEA: Permite que la tabla lea los datos de tus modelos
+    opens ni.edu.uam.registro_app.modelos to javafx.base;
+
     exports ni.edu.uam.registro_app;
 }
--- registro_app/src/main/java/ni/edu/uam/registro_app/controllers/EstudianteController.java
+++ registro_app/src/main/java/ni/edu/uam/registro_app/controllers/EstudianteController.java
@@ -1,7 +1,10 @@
 package ni.edu.uam.registro_app.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import ni.edu.uam.registro_app.dao.EstudianteDao;
 import ni.edu.uam.registro_app.modelos.Estudiante;
 
@@ -12,45 +15,53 @@ public class EstudianteController {
 
     EstudianteDao listado = new EstudianteDao();
 
+    // Lista especial para la tabla
+    private ObservableList<Estudiante> listaEstudiantesTabla = FXCollections.observableArrayList();
 
-    @FXML
-    private TextField txtNombres;
-    @FXML
-    private TextField txtApellidos;
-    @FXML
-    private TextField txtCarrera;
-    @FXML
-    private DatePicker dpFechaNac;
-    @FXML
-    private CheckBox chkTieneBeca;
-    @FXML
-    private Label lblRegistros;
-
-    //nuevos controles que son de los nuevos botones
+    @FXML private TextField txtNombres;
+    @FXML private TextField txtApellidos;
+    @FXML private DatePicker dpFechaNac;
+    @FXML private CheckBox chkTieneBeca;
+    @FXML private Label lblRegistros;
     @FXML private ComboBox<String> cmbCarrera;
     @FXML private RadioButton rbPresencial;
     @FXML private RadioButton rbVirtual;
     @FXML private ToggleGroup grupoModalidad;
     @FXML private ListView<String> lvActividades;
 
-    @FXML // arranca al abrir la ventana , es para configurar los controles
-    public void initialize(){
+    // NUEVOS CONTROLES DE LA TABLA
+    @FXML private TableView<Estudiante> tvEstudiantes;
+    @FXML private TableColumn<Estudiante, String> colNombres;
+    @FXML private TableColumn<Estudiante, String> colApellidos;
+    @FXML private TableColumn<Estudiante, String> colCarrera;
+    @FXML private TableColumn<Estudiante, String> colModalidad;
+    @FXML private TableColumn<Estudiante, LocalDate> colFechaNac;
+    @FXML private TableColumn<Estudiante, Boolean> colBeca;
+    @FXML private TableColumn<Estudiante, List<String>> colActividades;
 
-        cmbCarrera.getItems().addAll("Ingeneria en Sistemas","Diplomacia y Relaciones Internacionales", "Medicina", "Administracion de Empresas", "Diseño Grafico");
-        lvActividades.getItems().addAll("Futbol","Voleibol","Teatro","Ingles", "Jujitsu");
+    @FXML
+    public void initialize() {
+        cmbCarrera.getItems().addAll("Ingeniería en Sistemas", "Medicina", "Administración", "Diseño Gráfico");
+        lvActividades.getItems().addAll("Fútbol", "Voleibol", "Teatro", "Inglés");
         lvActividades.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         txtNombres.setOnAction(event -> txtApellidos.requestFocus());
-
         txtApellidos.setOnAction(event -> cmbCarrera.requestFocus());
-
         cmbCarrera.setOnAction(event -> dpFechaNac.requestFocus());
-
         dpFechaNac.setOnAction(event -> chkTieneBeca.requestFocus());
 
+        // CONFIGURACIÓN DE LA TABLA: Vinculamos cada columna a su variable en Estudiante.java
+        colNombres.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
+        colCarrera.setCellValueFactory(new PropertyValueFactory<>("carrera"));
+        colModalidad.setCellValueFactory(new PropertyValueFactory<>("modalidad"));
+        colFechaNac.setCellValueFactory(new PropertyValueFactory<>("fechaNacimiento"));
+        colBeca.setCellValueFactory(new PropertyValueFactory<>("tieneBeca"));
+        colActividades.setCellValueFactory(new PropertyValueFactory<>("actividades"));
 
+        // Le pasamos la lista observable a la tabla
+        tvEstudiantes.setItems(listaEstudiantesTabla);
     }
-
 
     @FXML
     protected void guardarOnClick(){
@@ -60,14 +71,11 @@ public class EstudianteController {
     }
 
     private void leerDatos(){
-        // Usamos las variables actualizadas con "s"
         String nombre = txtNombres.getText();
         String apellidos = txtApellidos.getText();
-        String carrera = cmbCarrera.getValue();
         LocalDate fechaNac = dpFechaNac.getValue();
         Boolean tieneBeca = chkTieneBeca.isSelected();
-
-
+        String carrera = cmbCarrera.getValue();
         String modalidad = rbPresencial.isSelected() ? "Presencial" : "Virtual";
         List<String> actividades = lvActividades.getSelectionModel().getSelectedItems();
 
@@ -75,23 +83,22 @@ public class EstudianteController {
     }
 
     private void agregarDatos(Estudiante estudiante){
-        listado.agregar(estudiante);
+        listado.agregar(estudiante); // Lo guarda en tu base de datos Dao
+        listaEstudiantesTabla.add(estudiante); // Lo muestra en la tabla visualmente
     }
 
     private void contarRegistros(){
         lblRegistros.setText("Registros almacenados: " + listado.obtenerRegistros().size());
     }
+
     private void limpiarCampos(){
         txtNombres.clear();
         txtApellidos.clear();
-
         dpFechaNac.setValue(null);
         chkTieneBeca.setSelected(false);
         cmbCarrera.setValue(null);
         rbPresencial.setSelected(true);
         lvActividades.getSelectionModel().clearSelection();
-
         txtNombres.requestFocus();
-
     }
 }
